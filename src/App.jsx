75eda0746a94419e40e505bdbbca28c5a8a61e7f
simplysamiul <simplysamiul@gmail.com>
2025-08-18 12:51:24 +0600

    create repo and make 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="font-secondary text-yellowOp">Welcome to donate</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
